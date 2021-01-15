024b3b3c6ffa272c7ad49a510c73ce9986b3cd30
delayedaa <delayedaa@gmail.com>
2021-01-15 08:51:44 -0600

    Create /rooms URL

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+
+class Room:
+    def __init__(self, number, room_type, beds, accessible, occupied, clean):
+        self.number = number
+        self.room_type = room_type
+        self.beds = beds
+        self.accessible = accessible
+        self.occupied = occupied
+        self.clean = clean
+
+
+rooms = [
+    Room(101, 'standard, street view', '1 king', True, False, True),
+    Room(204, 'standard, courtyard view', '2 queens', False, False, True),
+    Room(209, 'suite, street view', '1 king', False, True, False),
+]
+
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('<h1>Welcome!</h1>')
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def rooms_index(request):
+    return render(request, 'rooms_index.html', {'rooms': rooms})
