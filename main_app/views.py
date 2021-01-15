eaebae4b85e523456103b972a20042f49ed42941
delayedaa <delayedaa@gmail.com>
2021-01-15 10:14:39 -0600

    Set up admin interface

--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,13 @@
 from django.shortcuts import render
+from .models import Room
 # from django.http import HttpResponse
 
 
-class Room:
-    def __init__(self, number, room_type, beds, accessible, occupied, clean):
-        self.number = number
-        self.room_type = room_type
-        self.beds = beds
-        self.accessible = accessible
-        self.occupied = occupied
-        self.clean = clean
-
-
-rooms = [
-    Room(101, 'standard, street view', '1 king', True, False, True),
-    Room(204, 'standard, courtyard view', '2 queens', False, False, True),
-    Room(209, 'suite, street view', '1 king', False, True, False),
-]
+# rooms = [
+#     Room(101, 'standard, street view', '1 king', True, False, True),
+#     Room(204, 'standard, courtyard view', '2 queens', False, False, True),
+#     Room(209, 'suite, street view', '1 king', False, True, False),
+# ]
 
 
 # Create your views here.
@@ -31,4 +22,5 @@ def about(request):
 
 
 def rooms_index(request):
+    rooms = Room.objects.all()
     return render(request, 'rooms/index.html', {'rooms': rooms})
