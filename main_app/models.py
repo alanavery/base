7386fd1bdb37d9ceb1ab06e0374e869e4f6bed04
delayedaa <delayedaa@gmail.com>
2021-01-15 09:47:08 -0600

    Add Room model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Room(models.Model):
+    number = models.PositiveIntegerField()
+    room_type = models.CharField(max_length=100)
+    beds = models.CharField(max_length=100)
+    accessible = models.BooleanField()
+    occupied = models.BooleanField()
+    spotless = models.BooleanField()
+
+    def __str__(self):
+        return str(self.number)
