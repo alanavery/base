f3fa4aef07f259b0b7fbfb5a29ae896c0c803f39
delayedaa <delayedaa@gmail.com>
2021-01-16 16:25:50 -0600

    Add urls for adding, updating, and deleting rooms

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('rooms/', views.rooms_index, name='rooms_index'),
     path('rooms/<int:room_id>/', views.rooms_details, name='rooms_details'),
+    path('rooms/create/', views.RoomCreate.as_view(), name='rooms_create'),
+    path('rooms/<int:pk>/update/',
+         views.RoomUpdate.as_view(), name='rooms_update'),
+    path('rooms/<int:pk>/delete/',
+         views.RoomDelete.as_view(), name='rooms_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect
 from .models import Room
-# from django.http import HttpResponse
+
+# Create form views
 
 
-# rooms = [
-#     Room(101, 'standard, street view', '1 king', True, False, True),
-#     Room(204, 'standard, courtyard view', '2 queens', False, False, True),
-#     Room(209, 'suite, street view', '1 king', False, True, False),
-# ]
+class RoomCreate(CreateView):
+    model = Room
+    fields = '__all__'
+    success_url = '/rooms'
 
+
+class RoomUpdate(UpdateView):
+    model = Room
+    fields = ['beds', 'occupied', 'spotless']
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+        return HttpResponseRedirect(f'/rooms/{str(self.object.pk)}')
+
+
+class RoomDelete(DeleteView):
+    model = Room
+    success_url = '/rooms'
 
 # Create your views here.
 
