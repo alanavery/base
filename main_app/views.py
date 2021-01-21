2602119757fe8d6df0b63cf99c471c5ca807113f
delayedaa <delayedaa@gmail.com>
2021-01-21 14:42:59 -0600

    Create user logout system

--- main_app/views.py
+++ main_app/views.py
@@ -66,6 +66,11 @@ def login_view(request):
         return render(request, 'login.html', {'form': form})
 
 
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+
 def profile(request, username):
     user = User.objects.get(username=username)
     bookings = Booking.objects.filter(user=user)
