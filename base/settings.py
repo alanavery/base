828ec1b2c5f74a76e39ad1f58918f49f6c4b7537
delayedaa <delayedaa@gmail.com>
2021-01-27 11:00:20 -0600

    Update fixture

--- base/settings.py
+++ base/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import django_heroku
 import os
 from pathlib import Path
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-load_dotenv()
+# load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
